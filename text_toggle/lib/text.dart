efb989ec1c18c605d635a05c345cd96972a677f9
Jimmy <jimmylanbcn@gmail.com>
2021-04-25 23:09:44 -0400

    Update styling

--- text_toggle/lib/text.dart
+++ text_toggle/lib/text.dart
@@ -10,7 +10,7 @@ class TextDisplay extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(20),
       child: Center(
-        child: Text(text),
+        child: Text(text, style: TextStyle(fontSize: 24),),
       ),
     );
   }
